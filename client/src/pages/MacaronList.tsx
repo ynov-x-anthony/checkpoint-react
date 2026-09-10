7d146b058cd0363b3bfb3167553270bb8c2f6fc8
Adel Mahmoud <mahmoud.adel@ynov.com>
2026-09-10 16:18:26 +0200

    Etape 2- Step 1 and step 2 finsh

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,43 +1,67 @@
+import { useState, useEffect } from "react";
 import Macaron from "../components/Macaron";
 
-/* ************************************************************************* */
-const sampleMacarons: MacaronArray = [
-	{
-		id: 10,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "blue",
-		color2: "white",
-		color3: "red",
-		name: "France",
-	},
-	{
-		id: 11,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "yellow",
-		color2: "red",
-		color3: "black",
-		name: "Germany",
-	},
-	{
-		id: 27,
-		accessory_id: "5",
-		accessory: "christmas-candy",
-		color1: "yellow",
-		color2: "blue",
-		color3: "blue",
-		name: "Sweden",
-	},
-];
+// Typage pour l'étape 1
+type MacaronType = {
+  id: number;
+  accessory_id: string;
+  accessory: string;
+  color1: string;
+  color2: string;
+  color3: string;
+  name: string;
+};
 
-/* you can use sampleMacarons if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
+/* ************************************************************************* */
+const sampleMacarons = [
+    {
+        id: 10,
+        accessory_id: "4",
+        accessory: "agorski",
+        color1: "blue",
+        color2: "white",
+        color3: "red",
+        name: "France",
+    },
+    {
+        id: 11,
+        accessory_id: "4",
+        accessory: "agorski",
+        color1: "yellow",
+        color2: "red",
+        color3: "black",
+        name: "Germany",
+    },
+    {
+        id: 27,
+        accessory_id: "5",
+        accessory: "christmas-candy",
+        color1: "yellow",
+        color2: "blue",
+        color3: "blue",
+        name: "Sweden",
+    },
+];
 /* ************************************************************************* */
 
 function MacaronList() {
-	// Step 1: get all macarons
+    // Step 1: get all macarons
+    // Utilisation de useState pour stocker les macarons
+    const [macarons, setMacarons] = useState<MacaronType[]>([]);
 
+    // Utilisation de useEffect pour déclencher fetch au montage
+    useEffect(() => {
+        const fetchMacarons = async () => {
+            const response = await fetch("http://localhost:3310/api/macarons");
+            const data = await response.json();
+            
+            setMacarons(data);
+			//vérifier la data
+            console.info(data); 
+        };
+
+        fetchMacarons();
+    }, []);
 	// Step 3: get all accessories
 
 	// Step 5: create filter state
@@ -57,6 +81,11 @@ function MacaronList() {
 			</form>
 			<ul className="macaron-list" id="macaron-list">
 				{/* Step 2: repeat this block for each macaron */}
+				{macarons.map((macaron) => (
+                    <li key={macaron.id} className="macaron-item">
+                        <Macaron data={macaron} />
+                    </li>
+                ))}
 				{/* Step 5: filter macarons before repeating */}
 				<li className="macaron-item">
 					<Macaron data={sampleMacarons[0]} />
