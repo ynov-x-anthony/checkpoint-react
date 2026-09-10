e844937a4c522cc88f804a18693c6faa1986dae6
Kylian <kylian.soulet@ynov.com>
2026-09-10 14:13:15 +0200

    algo 1 finit et algo 2 commencÃ

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,15 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+    let count = 0;
+
+    for (const char of givenString) {
+        if (char === letter) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 export default countLetters;
--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,7 +11,10 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
+    if (size <= 0) {
+        return [];
+    }
+
   return [0, 1];
 }
 
