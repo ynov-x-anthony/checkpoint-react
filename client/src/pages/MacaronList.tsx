a8b4cd52f62ce8785e87a8a0fc0c2daf1b500018
Mathys.E <mathys.eustache@ynov.com>
2026-09-10 16:15:08 +0200

    step4

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -2,7 +2,8 @@ import Macaron from "../components/Macaron";
 import { useEffect, useState } from "react";
 
 /* ************************************************************************* */
-
+// Ajout du type défini dans ton Indice 2
+type AccessoryArray = { id: number; name: string; slug: string }[];
 /* you can use sampleMacarons if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
 /* ************************************************************************* */
@@ -23,6 +24,18 @@ function MacaronList() {
     
 
     // Step 3: get all accessories
+    const [accessories, setAccessories] = useState<AccessoryArray>([]);
+
+    useEffect(() => {
+        async function loadAccessories() {
+            const res = await fetch("http://localhost:3310/api/accessories");
+            const data = (await res.json()) as AccessoryArray;
+            setAccessories(data);
+            console.info("Accessoires de l'API :", data);
+        }
+        loadAccessories();
+    }, []);
+
     // Step 5: create filter state
 
     return (
@@ -35,11 +48,17 @@ function MacaronList() {
                     <select id="macaron-select">
                         <option value="">---</option>
                         {/* Step 4: add an option for each accessory */}
+                        {accessories.map((accessory) => (
+                            <option key={accessory.id} value={accessory.id}>
+                                {accessory.name}
+                            </option>
+                        ))}
                     </select>
                 </label>
             </form>
             <ul className="macaron-list" id="macaron-list">
                 {/* Step 2: repeat this block for each macaron */}
+                {/* Step 5: filter macarons before repeating */}
                 {macarons.map((macaron) => (
                     <li className="macaron-item" key={macaron.id}>
                         <Macaron data={macaron} />
