9364a711c1537b3d41aaa7f33b064f17fe7aa44e
Evan SEDDA <seddaevan@outlook.fr>
2026-09-10 15:35:27 +0200

    finished step 4

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -68,8 +68,11 @@ function MacaronList() {
 					{/* Step 5: use a controlled component for select */}
 					Filter by{" "}
 					<select id="macaron-select">
-						<option value="">---</option>
-						{/* Step 4: add an option for each accessory */}
+						<option value="1">Cherry</option>
+						<option value="2">Donut</option>
+						<option value="3">Chocolate</option>
+						<option value="4">agorski.dev</option>
+						<option value="5">Christmas Candy</option>
 					</select>
 				</label>
 			</form>
