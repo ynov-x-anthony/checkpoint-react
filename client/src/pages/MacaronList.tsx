ab320ccdd7a09f7036b9bf1626525dfb1619a20e
Cromca <fuentes.quentin.13@gmail.com>
2026-09-10 16:13:07 +0200

    etape 3 ,récuperation de tout les accessoires

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 
-// Step 2 : import à utiliser lors de l'affichage des macarons
-// import Macaron from "../components/Macaron";
+import Macaron from "../components/Macaron";
+
+type AccessoryArray = {
+  id: number;
+  name: string;
+  slug: string;
+}[];
 
 function MacaronList() {
   // Step 1: get all macarons
@@ -21,11 +26,33 @@ function MacaronList() {
         console.info("Macarons récupérés :", data);
       })
       .catch((error) => {
-        console.error("Erreur lors de la récupération :", error);
+        console.error("Erreur lors de la récupération des macarons :", error);
       });
   }, []);
 
   // Step 3: get all accessories
+  const [accessories, setAccessories] = useState<AccessoryArray>([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/accessories`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+
+        return response.json();
+      })
+      .then((data: AccessoryArray) => {
+        setAccessories(data);
+        console.info("Accessoires récupérés :", data);
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la récupération des accessoires :",
+          error,
+        );
+      });
+  }, []);
 
   // Step 5: create filter state
 
@@ -37,6 +64,10 @@ function MacaronList() {
         {macarons.length} macarons récupérés.
       </p>
 
+      <p className="center">
+        {accessories.length} accessoires récupérés.
+      </p>
+
       <form className="center">
         <label htmlFor="macaron-select">
           {/* Step 5: use a controlled component for select */}
@@ -45,12 +76,22 @@ function MacaronList() {
             <option value="">---</option>
 
             {/* Step 4: add an option for each accessory */}
+            {accessories.map((accessory) => (
+              <option key={accessory.id} value={accessory.id}>
+                {accessory.name}
+              </option>
+            ))}
           </select>
         </label>
       </form>
 
       <ul className="macaron-list" id="macaron-list">
         {/* Step 2: repeat this block for each macaron */}
+        {macarons.map((macaron) => (
+          <li className="macaron-item" key={macaron.id}>
+            <Macaron data={macaron} />
+          </li>
+        ))}
 
         {/* Step 5: filter macarons before repeating */}
       </ul>
