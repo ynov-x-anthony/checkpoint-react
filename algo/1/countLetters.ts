6e9a2f3af8c08f5551e5bf501a74e32c6efc52c9
Eddinee <edssee008@gmail.com>
2026-09-10 15:57:06 +0200

    test1 réussi

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -1,17 +1,7 @@
-/*
-Créé une fonction countLetters qui compte, dans une string donnée, le nombre de fois qu'une lettre apparait.
-
-Exemples :
-* "" et "a" -> 0
-* "a" et "a" -> 1
-* "aaaaabbbaa" et "a" -> 7
-* "bbacbaaa" et "c" -> 1
-* "bbcc" et "a" -> 0
-*/
-
-function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+export function countLetters(str: string): Record<string, number> {
+  const counts: Record<string, number> = {};
+  for (const char of str) {
+    counts[char] = (counts[char] || 0) + 1;
+  }
+  return counts;
 }
-
-export default countLetters;
