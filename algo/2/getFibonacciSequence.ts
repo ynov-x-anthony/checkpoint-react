c6c8475a91d2c1cdf581535fbc5b50565390352f
AyrtonBLD <ayrton.blondeau@ynov.com>
2026-09-10 14:39:23 +0200

    step 2 - getFibonacciSequence

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -1,18 +1,14 @@
 function getFibonacciSequence(size: number): number[] {
-  // Cas où la taille demandée est 0 ou négative
   if (size <= 0) {
     return [];
   }
 
-  // Cas où on ne demande que le premier terme
   if (size === 1) {
     return [0];
   }
 
-  // Initialisation avec les 2 premiers termes
   const sequence: number[] = [0, 1];
 
-  // Calcul des termes suivants jusqu'à atteindre la taille voulue
   for (let i = 2; i < size; i++) {
     const nextValue = sequence[i - 1] + sequence[i - 2];
     sequence.push(nextValue);
