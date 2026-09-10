0317c4f0bbb8a638670a02bfa1a9f443a62e57c8
CAILLOL <148053991+NicolasCAILLOL@users.noreply.github.com>
2026-09-10 14:24:22 +0200

    finished algo1.ts countLetters

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,13 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+    let number = 0;
+    for (let i = 0; i < givenString.length; i++) {
+        if (givenString[i] === letter) {
+            number++;
+        }
+    }
+  return number;
 }
 
 export default countLetters;
