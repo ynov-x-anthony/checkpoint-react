da847c160662fb3f25eac1bbf70cb25a544556ea
Kylian <kylian.soulet@ynov.com>
2026-09-10 17:01:18 +0200

    du 1 au 5

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,70 +1,74 @@
+import { useState, useEffect } from "react";
 import Macaron from "../components/Macaron";
 
-/* ************************************************************************* */
-const sampleMacarons: MacaronArray = [
-	{
-		id: 10,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "blue",
-		color2: "white",
-		color3: "red",
-		name: "France",
-	},
-	{
-		id: 11,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "yellow",
-		color2: "red",
-		color3: "black",
-		name: "Germany",
-	},
-	{
-		id: 27,
-		accessory_id: "5",
-		accessory: "christmas-candy",
-		color1: "yellow",
-		color2: "blue",
-		color3: "blue",
-		name: "Sweden",
-	},
-];
-
-/* you can use sampleMacarons if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
+type AccessoryArray = { id: number; name: string; slug: string }[];
 
 function MacaronList() {
-	// Step 1: get all macarons
+    // Step 1: get all macarons
+    const [macarons, setMacarons] = useState<MacaronArray>([]);
 
-	// Step 3: get all accessories
+    useEffect(() => {
+        fetch("http://localhost:3310/api/macarons")
+            .then((response) => response.json())
+            .then((data) => {
+                setMacarons(data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
 
-	// Step 5: create filter state
+    // Step 3: get all accessories
+    const [accessories, setAccessories] = useState<AccessoryArray>([]);
 
-	return (
-		<>
-			<h1>My macarons</h1>
-			<form className="center">
-				<label htmlFor="macaron-select">
-					{/* Step 5: use a controlled component for select */}
-					Filter by{" "}
-					<select id="macaron-select">
-						<option value="">---</option>
-						{/* Step 4: add an option for each accessory */}
-					</select>
-				</label>
-			</form>
-			<ul className="macaron-list" id="macaron-list">
-				{/* Step 2: repeat this block for each macaron */}
-				{/* Step 5: filter macarons before repeating */}
-				<li className="macaron-item">
-					<Macaron data={sampleMacarons[0]} />
-				</li>
-				{/* end of block */}
-			</ul>
-		</>
-	);
+    useEffect(() => {
+        fetch("http://localhost:3310/api/accessories")
+            .then((response) => response.json())
+            .then((data) => {
+                setAccessories(data as AccessoryArray);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    // Step 5: create filter state
+    const [selectedAccessory, setSelectedAccessory] = useState<string>("");
+
+    const filteredMacarons = selectedAccessory === ""
+        ? macarons
+        : macarons.filter(macaron => macaron.accessory_id === selectedAccessory);
+
+    return (
+        <>
+            <h1>My macarons</h1>
+            <form className="center">
+                <label htmlFor="macaron-select">
+                    {/* Step 5: use a controlled component for select */}
+                    Filter by{" "}
+                    <select
+                        id="macaron-select"
+                        value={selectedAccessory}
+                        onChange={(e) => setSelectedAccessory(e.target.value)}
+                    >
+                        <option value="">---</option>
+                        {/* Step 4: add an option for each accessory */}
+                        {accessories.map((accessory) => (
+                            <option key={accessory.id} value={accessory.id.toString()}>
+                                {accessory.name}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+            </form>
+            <ul className="macaron-list" id="macaron-list">
+                {/* Step 2: repeat this block for each macaron */}
+                {/* Step 5: filter macarons before repeating */}
+                {filteredMacarons.map((macaron) => (
+                    <li key={macaron.id}  className="macaron-item">
+                        <Macaron data={macaron} />
+                    </li>
+                ))}
+                {/* end of block */}
+            </ul>
+        </>
+    );
 }
 
 export default MacaronList;
