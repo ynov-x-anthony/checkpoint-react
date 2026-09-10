09e65d04614f71be72b94806b4696f9f12cf7ddc
Amir <mohandarezki.amir@ynov.com>
2026-09-10 15:48:33 +0200

    Exo 3 getPoints

--- algo/3/getPoints.ts
+++ algo/3/getPoints.ts
@@ -11,8 +11,19 @@ Pour exemple, si ta fonction recevait le tableau ci-dessous en paramètre, tu de
 */
 
 function getPoints(results: string[]): number {
-  // Ton code ici !
-  return 0;
+  let points = 0;
+
+  for (const result of results) {
+    const [ourScore, otherScore] = result.split(":").map(Number);
+
+    if (ourScore > otherScore) {
+      points += 3;
+    } else if (ourScore === otherScore) {
+      points += 1;
+    }
+  }
+
+  return points;
 }
 
 export default getPoints;
