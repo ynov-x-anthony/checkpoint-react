3a82e23422308b3057b04b84c72b2ec0361bdb41
H129hj <hugob@MacBook-Air-de-Hugo.local>
2026-09-10 16:51:55 +0200

    step 1 + 2 (fetch + map) finished

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,42 +1,23 @@
+import { useEffect, useState } from "react";
+
 import Macaron from "../components/Macaron";
-
-/* ************************************************************************* */
-const sampleMacarons: MacaronArray = [
-	{
-		id: 10,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "blue",
-		color2: "white",
-		color3: "red",
-		name: "France",
-	},
-	{
-		id: 11,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "yellow",
-		color2: "red",
-		color3: "black",
-		name: "Germany",
-	},
-	{
-		id: 27,
-		accessory_id: "5",
-		accessory: "christmas-candy",
-		color1: "yellow",
-		color2: "blue",
-		color3: "blue",
-		name: "Sweden",
-	},
-];
-
-/* you can use sampleMacarons if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
 
 function MacaronList() {
 	// Step 1: get all macarons
+	const [macarons, setMacarons] = useState<MacaronArray>([]);
+
+	useEffect(() => {
+		const fetchMacarons = async () => {
+			const response = await fetch(`${import.meta.env.VITE_API_URL}/api/macarons`);
+			const data = (await response.json()) as MacaronArray;
+
+			console.info(data);
+
+			setMacarons(data);
+		};
+
+		fetchMacarons();
+	}, []);
 
 	// Step 3: get all accessories
 
@@ -58,9 +39,11 @@ function MacaronList() {
 			<ul className="macaron-list" id="macaron-list">
 				{/* Step 2: repeat this block for each macaron */}
 				{/* Step 5: filter macarons before repeating */}
-				<li className="macaron-item">
-					<Macaron data={sampleMacarons[0]} />
-				</li>
+				{macarons.map((macaron) => (
+					<li className="macaron-item" key={macaron.id}>
+						<Macaron data={macaron} />
+					</li>
+				))}
 				{/* end of block */}
 			</ul>
 		</>
