afb3788f1897739f24c9e319135b9d604c3e24a9
Nohan <nohan.reis@ynov.com>
2026-09-10 14:49:49 +0200

    finished step 2

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,41 +1,6 @@
 import { useEffect, useState } from "react";
 import Macaron from "../components/Macaron";
 
-/* ************************************************************************* */
-const sampleMacarons: MacaronArray = [
-	{
-		id: 10,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "blue",
-		color2: "white",
-		color3: "red",
-		name: "France",
-	},
-	{
-		id: 11,
-		accessory_id: "4",
-		accessory: "agorski",
-		color1: "yellow",
-		color2: "red",
-		color3: "black",
-		name: "Germany",
-	},
-	{
-		id: 27,
-		accessory_id: "5",
-		accessory: "christmas-candy",
-		color1: "yellow",
-		color2: "blue",
-		color3: "blue",
-		name: "Sweden",
-	},
-];
-
-/* you can use sampleMacarons if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
-
 function MacaronList() {
 	// Step 1: get all macarons
 	const [macarons, setMacarons] = useState<MacaronArray>([]);
@@ -47,8 +12,6 @@ function MacaronList() {
 				setMacarons(data);
 			});
 	}, []);
-
-	console.info(macarons);
 
 	// Step 3: get all accessories
 
@@ -68,12 +31,12 @@ function MacaronList() {
 				</label>
 			</form>
 			<ul className="macaron-list" id="macaron-list">
-				{/* Step 2: repeat this block for each macaron */}
 				{/* Step 5: filter macarons before repeating */}
-				<li className="macaron-item">
-					<Macaron data={sampleMacarons[0]} />
-				</li>
-				{/* end of block */}
+				{macarons.map((macaron) => (
+					<li className="macaron-item" key={macaron.id}>
+						<Macaron data={macaron} />
+					</li>
+				))}
 			</ul>
 		</>
 	);
