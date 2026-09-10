fb9f92b07557532b838bf19b3808eefa0dfc7e45
Sam <samuel.berard@ynov.com>
2026-09-10 16:06:27 +0200

    finished step 1 & 2

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -1,6 +1,6 @@
 import Macaron from "../components/Macaron";
+import { useEffect, useState } from "react";
 
-/* ************************************************************************* */
 const sampleMacarons: MacaronArray = [
 	{
 		id: 10,
@@ -34,37 +34,38 @@ const sampleMacarons: MacaronArray = [
 /* you can use sampleMacarons if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
 /* ************************************************************************* */
-
 function MacaronList() {
 	// Step 1: get all macarons
+	const [macarons, setMacarons] = useState<MacaronArray>([]);
 
-	// Step 3: get all accessories
-
-	// Step 5: create filter state
+	useEffect(() => {
+		fetch(`${import.meta.env.VITE_API_URL}/api/macarons`)
+			.then((res) => res.json())
+			.then((data: MacaronArray) => setMacarons(data))
+			.catch((err) => console.error(err));
+	}, []);
 
 	return (
 		<>
 			<h1>My macarons</h1>
 			<form className="center">
 				<label htmlFor="macaron-select">
-					{/* Step 5: use a controlled component for select */}
 					Filter by{" "}
 					<select id="macaron-select">
 						<option value="">---</option>
-						{/* Step 4: add an option for each accessory */}
 					</select>
 				</label>
 			</form>
 			<ul className="macaron-list" id="macaron-list">
-				{/* Step 2: repeat this block for each macaron */}
-				{/* Step 5: filter macarons before repeating */}
-				<li className="macaron-item">
-					<Macaron data={sampleMacarons[0]} />
-				</li>
-				{/* end of block */}
+				{macarons.map((macaron) => (
+					<li className="macaron-item" key={macaron.id}>
+						<Macaron data={macaron} />
+					</li>
+				))}
 			</ul>
 		</>
 	);
 }
 
 export default MacaronList;
+
