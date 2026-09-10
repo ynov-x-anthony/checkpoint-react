be1d029a5215abde2ab37563a06a5c9d7a0deed4
basttouuu <hug0.paulier83@gmail.com>
2026-09-10 14:12:02 +0200

    étape 1 fini

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,14 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let resultat = 0
+  for (let i = 0; i < givenString.length ; i++ ){
+    if (givenString[i] == letter){
+      resultat++
+    }
+  }
+
+  return resultat;
 }
 
 export default countLetters;
