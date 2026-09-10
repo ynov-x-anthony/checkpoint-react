bacc9b902cb726e4d43e78977c0256ebcca4d2e5
Adel Mahmoud <mahmoud.adel@ynov.com>
2026-09-10 14:34:24 +0200

    finished algo 2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,16 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+  //Gerer les cas particuliers
+  if (size <= 0) return [];
+  if (size === 1) return [0];
+  //Indexer le tableau pour commencer 
+  const sequence: number[]= [0, 1];
+  //La suite de Fibonacci est le nombre actuelle et la somme des 2 precedents (F(x)=F(x-1)+F(x-2)) pour realiser ca on vas faire une boucle avec iterations ou i=2
+  for (let i = 2; i < size; i++){
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  return sequence;
 }
 
 export default getFibonacciSequence;
