c87973e11accd40d3a1aa772fcba712e5b6f2e1c
Mathys.E <mathys.eustache@ynov.com>
2026-09-10 14:01:27 +0200

    countLetters

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,10 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Ton code ici !
-  return 0;
+  if (!letter) return 0;
+
+  return givenString.split(letter).length-1;
+  
 }
 
 export default countLetters;
--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -12,7 +12,12 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 
 function getFibonacciSequence(size: number): number[] {
   // Ton code ici !
-  return [0, 1];
+  if (size <= 0) return [];
+  
+  const fib = [0, 1];
+  while (fib.length < size) fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  
+  return fib.slice(0, size);
 }
 
 export default getFibonacciSequence;
