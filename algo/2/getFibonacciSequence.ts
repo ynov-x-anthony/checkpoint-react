1e7caa0a64187df40fddc08ea216ceba9812ab08
Kylian <kylian.soulet@ynov.com>
2026-09-10 14:28:46 +0200

    algo 2 fibonacci fini :)

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -15,7 +15,17 @@ function getFibonacciSequence(size: number): number[] {
         return [];
     }
 
-  return [0, 1];
+    if (size === 1) {
+        return [0];
+    }
+
+    const tab = [0, 1];
+
+    for (let i = 2; i < size; i++) {
+        tab.push(tab[i - 1] + tab[i - 2]);
+    }
+
+  return tab;
 }
 
 export default getFibonacciSequence;
