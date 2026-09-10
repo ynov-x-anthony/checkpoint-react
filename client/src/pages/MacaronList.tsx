4e5f8c8a182b57d70f4d90b2134766c216538dd2
Nohan <nohan.reis@ynov.com>
2026-09-10 15:39:06 +0200

    finished step 4

--- client/src/pages/MacaronList.tsx
+++ client/src/pages/MacaronList.tsx
@@ -26,8 +26,6 @@ function MacaronList() {
 			});
 	}, []);
 
-	console.info(accessories);
-
 	// Step 5: create filter state
 
 	return (
@@ -39,7 +37,11 @@ function MacaronList() {
 					Filter by{" "}
 					<select id="macaron-select">
 						<option value="">---</option>
-						{/* Step 4: add an option for each accessory */}
+						{accessories.map((accessory) => (
+							<option value={accessory.id} key={accessory.id}>
+								{accessory.name}
+							</option>
+						))}
 					</select>
 				</label>
 			</form>
