72ac21837989379a6785cf3365f268f27eb155ab
Slypot <sylvain.pottier@ynov.com>
2026-09-10 14:33:18 +0200

    algo:test:2 Validé

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -1,18 +1,17 @@
-/*
-Créé une fonction getFibonacciSequence qui prend un nombre n en paramètre et retourne un tableau contenant les n premiers nombres de la suite de Fibonacci.
-
-Détails
-
-* La suite de Fibonacci commence par les nombres 0 et 1.
-* Chaque nombre suivant est la somme des deux nombres précédents.
-* Par exemple, pour n = 5, la fonction devrait retourner [0, 1, 1, 2, 3].
-
-Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
-*/
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
-}
+  if (size <= 0) {
+    return [];
+  }
+  if (size === 1) {
+    return [0];
+  }
 
+const tab = [0, 1];
+
+for (let i=2; i < size; i++) {
+    tab.push(tab[i-1] + tab[i-2]);
+  }
+return tab;
+}
 export default getFibonacciSequence;
