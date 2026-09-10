c17b391cb0963b5303d34004970b2562119e2012
Lisa Payan <lisa.payan@ynov.com>
2026-09-10 14:10:03 +0200

    feat: algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,13 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let counter = 0;
+  for (let i = 0; i < givenString.length; i ++) {
+    if ( givenString[i] == letter) {
+      counter ++;
+    }
+  }
+  return counter;
 }
 
 export default countLetters;
