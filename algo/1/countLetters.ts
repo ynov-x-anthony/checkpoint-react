536ad57837aee7ca1d6747d73df8a90ec061ff6e
H129hj <hugob@MacBook-Air-de-Hugo.local>
2026-09-10 14:33:03 +0200

    finished algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,7 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  return [...givenString].filter((character) => character === letter).length;
 }
 
 export default countLetters;
