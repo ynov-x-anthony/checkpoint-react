444e3aee7c0484d4dc7a969bb168c40d85d731e4
Evan SEDDA <seddaevan@outlook.fr>
2026-09-10 13:48:35 +0200

    finished algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,13 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let count = 0
+  for (let index = 0; index < givenString.length; index++) {
+    if (letter == givenString[index]) {
+      count ++
+    }
+  }
+  return count;
 }
 
 export default countLetters;
