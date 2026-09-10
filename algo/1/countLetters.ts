c41481cdd146369d533e300cdd10eaf65043b0bc
Cromca <fuentes.quentin.13@gmail.com>
2026-09-10 14:03:26 +0200

    ajout d'algo1.ts avec test reussi

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,13 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let count = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      count++;
+    }
+  }
+  return count;
 }
 
 export default countLetters;
