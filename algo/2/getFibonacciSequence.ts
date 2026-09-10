3b836b665c081778c1a0e18be26dade5c3950dd4
Cromca <fuentes.quentin.13@gmail.com>
2026-09-10 14:12:52 +0200

    ajout de l'algo 2 algo2.ts avec test reussi

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,18 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
-}
+if (size <= 0) {
+    return [];
 
+}
+if (size === 1) {
+    return [0];
+}
+const sequence: number[] = [0, 1];
+for (let i = 2; i < size; i++) {
+    const nextNumber = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextNumber);
+}
+return sequence;
+}
 export default getFibonacciSequence;
